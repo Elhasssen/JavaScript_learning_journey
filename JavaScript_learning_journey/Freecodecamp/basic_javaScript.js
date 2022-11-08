f20580354190286c2cf33baaef123ef305a8f26b
ElHassen <Boumeddiene.elhassen@gmail.com>
2022-11-08 13:37:48 +0100

    updated

--- JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
+++ JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
@@ -982,3 +982,86 @@
 //     i++;
 
 // } while (i < 5); 
+// -----------------------
+// Replace loops with recursion 
+// Recursion is the concept that a function can be expreseed in terms
+// of itself. To help understand this, start by thinking about the 
+// following task: multiply the first n elements of an array to create
+// the product of those elements, using a for loop, you could do this :
+
+// function multiply(arr,n) {
+//     let product = 1;
+//     for (let i = 0; i < n; i++) {
+//         product *= arr[i];
+//     }
+//     return product
+// }
+// // this can be done this way : 
+// function multiply(arr,n) {
+//     if (n <= 0) {
+//         return 1;
+//     } else {
+//         return multiply(arr, n - 1) * arr[n - 1];
+//     }
+// }
+// --------------------------
+// profile lookup 
+// we have an array of objects representing diffrent people in our contacts list
+// a lookupProfile function that takes name and property as arguments 
+// been pre-written for you
+// The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+
+// If both are true, then return the "value" of that property.
+
+// If name does not correspond to any contacts then return the 
+// string No such contact.
+
+// If prop does not correspond to any valid properties of a 
+// contact found to match name then return the string No such property.
+
+// Setup
+// const contacts = [
+//     {
+//       firstName: "Akira",
+//       lastName: "Laine",
+//       number: "0543236543",
+//       likes: ["Pizza", "Coding", "Brownie Points"],
+//     },
+//     {
+//       firstName: "Harry",
+//       lastName: "Potter",
+//       number: "0994372684",
+//       likes: ["Hogwarts", "Magic", "Hagrid"],
+//     },
+//     {
+//       firstName: "Sherlock",
+//       lastName: "Holmes",
+//       number: "0487345643",
+//       likes: ["Intriguing Cases", "Violin"],
+//     },
+//     {
+//       firstName: "Kristian",
+//       lastName: "Vos",
+//       number: "unknown",
+//       likes: ["JavaScript", "Gaming", "Foxes"],
+//     },
+//   ];
+  
+//   function lookUpProfile(name, prop) {
+//     // Only change code below this line
+//     for (let i = 0; i < contacts.length; i++){
+//       if (contacts[i].firstName == name) {
+//         if (contacts[i].hasOwnProperty(prop)) {
+//           return contacts[i][prop];
+//         } else {
+//           return 'No such property';
+//         }
+//       }
+  
+//     }
+//     return 'No such contact';
+//     // Only change code above this line
+//   }
+  
+//   lookUpProfile("Akira", "likes");
+// -------------------------------------------------------
