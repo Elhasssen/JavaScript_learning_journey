3ee8f9f6ae2c7e35320b7c4d5247b3304dd08be4
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-02-11 19:50:32 +0100

    const

--- JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
+++ JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
@@ -1154,5 +1154,19 @@
 // compare scopes of the vat and let keywords
 // When you declare a variable with the let keyword inside a block, statement, 
 // or expression, its scope is limited to that block, statement, or expression.
+// ---------------------------
+// mutate and array decalred with const
+// ----------------- 
+// it is important to understand that objects (including arrays and functions) assigned to a variable using const are still mutable. Using the const declaration 
+// only prevents reassignment of the variable identifier.
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   // Only change code below this line
+//   s[0] = 2;
+//   s[1] = 5;
+//   s[2] = 7;
+//   // Using s = [2, 5, 7] would be invalid
 
-
+//   // Only change code above this line
+// }
+// editInPlace();
