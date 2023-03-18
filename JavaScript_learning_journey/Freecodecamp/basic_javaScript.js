3de2cbe108fcafab9036beaf0863cb06f75f65d8
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-18 15:21:28 +0100

    getters and setters

--- JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
+++ JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
@@ -1291,4 +1291,29 @@
 //     }
 //   }
 // ------------------------------ 
-
+// Use getters and setter to control access to an object
+// Getter functions are meant to simply return (get) the value of an object's private variable
+// to the user without the user directly accessing the private variable.
+//Setter functions are meant to modify (set) the value of an object's private variable based on 
+// the value passed into the setter function. This change could involve calculations, or even 
+//overwriting the previous value completely.
+// example:
+// class Thermostat{
+//     constructor(temperature){
+//       this._temperature = temperature;
+//     }
+//     get temperature(){
+//       return (this._temperature - 32) * 5/9;
+//     }
+//     set temperature(newTemperature){
+//       this._temperature = (newTemperature * 9) / 5 + 32; 
+//     }
+//   }
+//   // Only change code above this line
+  
+//   const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+//   let temp = thermos.temperature; // 24.44 in Celsius
+//   thermos.temperature = 26;
+//   temp = thermos.temperature; // 26 in Celsius
+//----------------------------------------------------
+//
