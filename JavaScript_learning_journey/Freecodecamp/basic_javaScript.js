9b636371c310fdac61cca6d0718746b0c908c306
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-18 16:01:14 +0100

    default export and import

--- JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
+++ JavaScript_learning_journey/Freecodecamp/basic_javaScript.js
@@ -1316,4 +1316,36 @@
 //   thermos.temperature = 26;
 //   temp = thermos.temperature; // 26 in Celsius
 //----------------------------------------------------
-//
+// Exporting to share a code block
+// const uppercaseString = (string) => {
+//   return string.toUpperCase();
+// }
+
+// const lowercaseString = (string) => {
+//   return string.toLowerCase()
+// }
+// export {uppercaseString,lowercaseString};
+//----------------------------------------
+// Reuse JavaScript Code Using import
+// import { uppercaseString, lowercaseString} from './string_functions.js'
+// Only change code above this line
+
+// uppercaseString("hello");
+// lowercaseString("WORLD!");
+// ---------------------
+// Use * to Import Everything from a File
+// import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+// stringFunctions.uppercaseString("hello");
+// stringFunctions.lowercaseString("WORLD!");
+// Create an Export Fallback with export default
+// export default function subtract(x, y) {
+//   return x - y;
+// }
+//------------------------
+// Import a Default Export
+// import subtract from "./math_functions.js"  
+// // Only change code above this line
+
+// subtract(7,4);
