cf51b0193670d3488d8138ef9db82d4eef28fc95
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-20 15:48:17 +0100

    modify

--- JavaScript_learning_journey/Freecodecamp/ES6.js
+++ JavaScript_learning_journey/Freecodecamp/ES6.js
@@ -5,11 +5,244 @@
 // you declare a variable with let keyword inside a block, statement or expression
 // its scope is limited to that bloc, statement, or expression.
 // for example : 
-var numArray = [];
-for (var i=0; i < 3; i++){
-    numArray.push(i);
-}
-console.log(numArray);
-console.log(i);
+// var numArray = [];
+// for (var i=0; i < 3; i++){
+//     numArray.push(i);
+// }
+// console.log(numArray);
+// console.log(i);
 // Here the console will display the values [0,1.2] and 3
+//--------------------------------------------------------
+// compare scopes of the vat and let keywords
+// When you declare a variable with the let keyword inside a block, statement, 
+// or expression, its scope is limited to that block, statement, or expression.
+// ---------------------------
+// mutate and array decalred with const
+// ----------------- 
+// it is important to understand that objects (including arrays and functions) assigned to a variable using const are still mutable. Using the const declaration 
+// only prevents reassignment of the variable identifier.
+// const s = [5, 7, 2];
+// function editInPlace() {
+//   // Only change code below this line
+//   s[0] = 2;
+//   s[1] = 5;
+//   s[2] = 7;
+//   // Using s = [2, 5, 7] would be invalid
+
+//   // Only change code above this line
+// }
+// editInPlace();
+// --------------------
+// prevent objects mutation 
+// 
+// 
+// let obj = {
+//   name:"FreeCodeCamp",
+//   review:"Awesome"
+// };
+// Object.freeze(obj);
+// obj.review = "bad";
+// obj.newProp = "Test";
+// console.log(obj); 
+// Object.freeze will prevent mutation
+// -------------------------
+// When there is no function body, and only a return value, arrow function 
+// syntax allows you to omit the keyword return as well as the brackets 
+// surrounding the code. This helps simplify smaller functions into one-line 
+// statements:
+// const myFunc = function() {
+//   const myVar = "value";
+//   return myVar;
+// }
+// thjis can be written to this 
+// const myFunc = () => "Value" ;
+// ------------------------------------
+// Set Default Parameters for Your Functions
+// const greeting = (name = "Anonymous") => "Hello " + name;
+
+// console.log(greeting("John"));
+// console.log(greeting());
+// he default parameter kicks in when the argument is not specified (it is undefined).
+//-------------------------------------------
+// Use the Rest Parameter with Function Parameters
+// function howMany(...args) {
+//   return "You have passed " + args.length + " arguments.";
+// }
+// console.log(howMany(0, 1, 2));
+// console.log(howMany("string", null, [1, 2, 3], { }));
+// The console would display the strings You have passed 3 arguments. and You have passed 4 arguments..
+// -----------------------------------------------------------------
+// Use the Spread Operator to Evaluate Arrays In-Place
+//...arr returns an unpacked array. In other words, it spreads the array. However, the spread operator only works in-place,
+// const arr = [6, 89, 3, 45];
+// const maximus = Math.max(...arr);
+// New ways to extract values from objects 
+// using the destucturing assignment
+// const user = {name = "hassen", age = 24};
+// const  {name, age} = user;
+// -----------------------------------------------------------------
+// Destructuring allows you to assign a new variable name when extracting values. You can do this by 
+// putting the new name after a colon when assigning the value.
+// const user = { name: 'John Doe', age: 34 };
+// const { name: userName, age: userAge } = user;
+// --------------------------------------------------------
+// Use Destructuring Assignment to Assign Variables from Nested Objects
+// const user = {
+//   johnDoe: { 
+//     age: 34,
+//     email: 'johnDoe@freeCodeCamp.com'
+//   }
+// };
+// to : 
+// const { johnDoe: { age, email }} = user;
+// or with diffrent names 
+// const {johnDoe : {age : userAge, email : UserEmail}} = user;
+//---------------------------------------
+// Use Destructuring Assignment to Assign Variables from Arrays
+// const [a, b] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b);
+// Destructuring via rest elements
+// The result is similar to Array.prototype.slice(), as shown below:
+// const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+// console.log(a, b);
+// console.log(arr);
+//-------------------------------------------
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// const profileUpdate = (profileData) => {
+//   const { name, age, nationality, location } = profileData;
+
+// }
+// to :
+// This effectively destructures the object sent into the function. This 
+// can also be done in-place:
+// const profileUpdate = ({ name, age, nationality, location }) => { }
+// ----------------------------------
+// Create Strings using Template Literals
+// A new feature of ES6 is the template literal. This 
+// is a special type of string that makes creating complex strings easier.
+// const greeting = `Hello, my name is ${person.name}!
+// I am ${person.age} years old.`;
+//-----------------------------------------
+// rite Concise Object Literal Declarations Using Object Property Shorthand
+// const getMousePosition = (x, y) => ({
+//     x: x,
+//     y: y
+//   });
+// to this fd
+// const getMousePosition = (x, y) => ({ x, y });
+// ---------------------------------------
+// using class ystax to define a constructor function 
+// In ES5, an object can be created by defining a constructor function and using the new keyword to instantiate the object.
+
+// In ES6, a class declaration has a constructor method that is invoked with the new keyword. 
+// If the constructor method is not explicitly defined, then it is implicitly defined with no arguments.
+// // Explicit constructor
+// class SpaceShuttle {
+//     constructor(targetPlanet) {
+//       this.targetPlanet = targetPlanet;
+//     }
+//     takeOff() {
+//       console.log("To " + this.targetPlanet + "!");
+//     }
+//   }
+  
+//   // Implicit constructor 
+//   class Rocket {
+//     launch() {
+//       console.log("To the moon!");
+//     }
+//   }
+// ------------------------------ 
+// Use getters and setter to control access to an object
+// Getter functions are meant to simply return (get) the value of an object's private variable
+// to the user without the user directly accessing the private variable.
+//Setter functions are meant to modify (set) the value of an object's private variable based on 
+// the value passed into the setter function. This change could involve calculations, or even 
+//overwriting the previous value completely.
+// example:
+// class Thermostat{
+//     constructor(temperature){
+//       this._temperature = temperature;
+//     }
+//     get temperature(){
+//       return (this._temperature - 32) * 5/9;
+//     }
+//     set temperature(newTemperature){
+//       this._temperature = (newTemperature * 9) / 5 + 32; 
+//     }
+//   }
+//   // Only change code above this line
+  
+//   const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+//   let temp = thermos.temperature; // 24.44 in Celsius
+//   thermos.temperature = 26;
+//   temp = thermos.temperature; // 26 in Celsius
+//----------------------------------------------------
+// Exporting to share a code block
+// const uppercaseString = (string) => {
+//   return string.toUpperCase();
+// }
+
+// const lowercaseString = (string) => {
+//   return string.toLowerCase()
+// }
+// export {uppercaseString,lowercaseString};
+//----------------------------------------
+// Reuse JavaScript Code Using import
+// import { uppercaseString, lowercaseString} from './string_functions.js'
+// Only change code above this line
+
+// uppercaseString("hello");
+// lowercaseString("WORLD!");
+// ---------------------
+// Use * to Import Everything from a File
+// import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+// stringFunctions.uppercaseString("hello");
+// stringFunctions.lowercaseString("WORLD!");
+// Create an Export Fallback with export default
+// export default function subtract(x, y) {
+//   return x - y;
+// }
+//------------------------
+// Import a Default Export
+// import subtract from "./math_functions.js"  
+// // Only change code above this line
+
+// subtract(7,4);
+//----------------------
+// JavaScript Promise
+// A promise in JavaScript is exactly what it sounds like - you use it to make a promise 
+// to do something, usually asynchronously. When the task completes, you either fulfill 
+// your promise or fail to do so. Promise is a constructor function, so you need to use 
+// the new keyword to create one.
+// const myPromise = new Promise((resolve, reject) => {
+
+// });
+// ---------------------------
+// Complete a Promise with resolve and reject
+//const myPromise = new Promise((resolve, reject) => {
+    // if(condition here) {
+    //     resolve("Promise was fulfilled");
+    //   } else {
+    //     reject("Promise was rejected");
+    //   }
+    // });
+//----------------------------------
+// Handle a Fulfilled Promise with then
+// const makeServerRequest = new Promise((resolve, reject) => {
+//     // responseFromServer is set to true to represent a successful response from a server
+//     let responseFromServer = true;
+      
+//     if(responseFromServer) {
+//       resolve("We got the data");
+//     } else {  
+//       reject("Data not received");
+//     }
+//   });
+  
+//   makeServerRequest.then(result => {
+//     console.log(result)
+//   });
 
---
+++ JavaScript_learning_journey/Freecodecamp/reguler_expressions.js
@@ -0,0 +1,110 @@
+// Regulat Expresions
+// JavaScript has multiple ways to use regexes. One way to test a regex 
+// is using the .test() method. The .test() method takes the regex,
+// let myString = "Hello, World!";
+// let myRegex = /Hello/;
+// let result = myRegex.test(myString); // Change this line
+// the test ;ethod here returns true
+//-------------------------------------
+// Match literal strings
+// n the last challenge, you searched for the word Hello using the regular expression /Hello/.
+// That regex searched for a literal match of the string Hello. Here's another example searching 
+// for a literal match of the string Kevin:
+// let testStr = "Hello, my name is Kevin.";
+// let testRegex = /Kevin/;
+// testRegex.test(testStr);
+// this test will retrun True
+// match a literal string with differenet ^possibilities 
+// let petString = "James has a pet cat.";
+// let petRegex = /dog|cat|bird|fish/; // Change this line
+// let result = petRegex.test(petString);
+// using "|" the alternation operator
+// ------------------------
+// igonre a case while matching
+// let myString = "freeCodeCamp";
+// let fccRegex = /freecodecamp/i; // Change this line
+// let result = fccRegex.test(myString);
+//------------------------------------------
+// Extracting matches 
+// you have only been checking if a pattern exists or not within a string. You can also 
+// extract the actual matches you found with the .match() method.
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line
+//-------------------------------------------
+// Finding more than the first match
+// let twinkleStar = "Twinkle, twinkle, little star";
+// let starRegex = /twinkle/gi; // Change this line
+// let result = twinkleStar.match(starRegex); // Change this line
+// using the global search 'g' and igonoring the case with 'i'
+//---------------------------------------------------------
+// match anything with wildcard period 
+// // let exampleStr = "Let's have fun with regular expressions!";
+// let unRegex = /.un./; // Change this line
+// let result = unRegex.test(exampleStr);
+// this will match word like, sun, run, nun, fun
+//----------------------------
+// Match single chartcher with multiple possibilities 
+// You can search for a literal pattern with some flexibility with character classes. 
+// Character classes allow you to define a group of characters you wish to match by placing 
+// them inside square ([ and ]) brackets.
+// in this example we want to match all the vowels inside this string 
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// so we put the class of all the vowels wanted to be matched, by ignoring the case by 'i' and searching more tha
+// than on match using the global match 'g' 
+// let vowelRegex = /[aeiou]/ig; // Change this line
+// let result = quoteSample.match(vowelRegex); 
+// console.log(result);// Change this line
+// -----------------------------------
+// match Letters of the alphabet
+// Inside a character set, you can define a range of 
+// characters to match using a hyphen character: -.
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/ig; // Change this line
+// let result = quoteSample.match(alphabetRegex); // Change this line
+// this will match all the alphabets with uppercase aswell 
+// and will match more than one match
+//--------------------------------------------------------
+// match numbers and letters of the alphabet
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/ig; // Change this line
+// let result = quoteSample.match(myRegex);
+// -----------------------------------------
+// Match single characters not specified 
+// To create a negated character set, you place a caret character (^) after the opening bracket 
+// and before the characters you do not want to match.
+// let quoteSample = "6 abs";
+// let myRegex = /[^aeiou0-5]/ig; // Change this line
+// let result = quoteSample.match(myRegex);
+// console.log(result) // Change this line
+// --------------------------------
+// Match characters that occur one or more times 
+// You can use the + character to check if that is the case. Remember, 
+// the character or pattern has to be present consecutively. That is, 
+// the character has to repeat one after the other.
+// let difficultSpelling = "Mississippi";
+// let myRegex = /s+/g // Change this line
+// let result = difficultSpelling.match(myRegex);
+// console.log(result);
+// -------------------------------
+// Match Characters that Occur Zero or More Times
+// let soccerWord = "gooooooooal!";
+// let gPhrase = "gut feeling";
+// let oPhrase = "over the moon";
+// let goRegex = /go*/;
+// soccerWord.match(goRegex);
+// gPhrase.match(goRegex);
+// oPhrase.match(goRegex);
+// In order, the three match calls would return the values ["goooooooo"], ["g"], and null.
+// --------------------------------------------------------------------------------------------
+// Find Characters with Lazy Matching
+// In regular expressions, a greedy match finds the longest possible part of a string that fits 
+// the regex pattern and returns it as a match. The alternative is called a lazy match, which 
+// finds the smallest possible part of the string that satisfies the regex pattern.
+// using '?' 
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<[a-z0-9]*?>/; // Change this line
+// let result = text.match(myRegex);
+// console.log(result)
+// The output is <h1> 
+// ---------------------------------------------------
