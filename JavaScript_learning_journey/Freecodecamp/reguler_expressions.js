603dc91cce876385fd49474c957d03c30d1de6b9
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-21 17:50:34 +0100

    Specify Exact Number of Matches

--- JavaScript_learning_journey/Freecodecamp/reguler_expressions.js
+++ JavaScript_learning_journey/Freecodecamp/reguler_expressions.js
@@ -219,3 +219,14 @@
 // multipleA.test(A2);
 // multipleA.test(A100);
 // In order, the three test calls would return true, false, and true.
+// Specifyu Exact number of matches
+// To specify a certain number of patterns, just have that one number between the curly brackets.
+// For example, to match only the word hah with the letter a 3 times, 
+// your regex would be /ha{3}h/.
+// let A4 = "haaaah";
+// let A3 = "haaah";
+// let A100 = "h" + "a".repeat(100) + "h";
+// let multipleHA = /ha{3}h/;
+// multipleHA.test(A4);
+// multipleHA.test(A3);
+// multipleHA.test(A100);
