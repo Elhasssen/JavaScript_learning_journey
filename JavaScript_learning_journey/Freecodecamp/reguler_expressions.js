1f3cf289f530e6ef7184ac47a3db691bfc7a158e
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-22 21:36:53 +0100

    Use Capture Groups to Search and Replace

--- JavaScript_learning_journey/Freecodecamp/reguler_expressions.js
+++ JavaScript_learning_journey/Freecodecamp/reguler_expressions.js
@@ -281,4 +281,16 @@
 // repeatRegex.test(repeatStr); // Returns true
 // repeatStr.match(repeatRegex); // Returns ["row row row", "row"]
 //----------------------------------------
-// 
+// Use Capture Groups to Search and Replace
+//You can search and replace text in a string using .replace() on a string. 
+// The inputs for .replace() is first the regex pattern you want to search for. 
+// The second parameter is the string to replace the match or a function to do something.
+// let wrongText = "The sky is silver.";
+// let silverRegex = /silver/;
+// wrongText.replace(silverRegex, "blue");
+// The replace call would return the string The sky is blue..
+
+// You can also access capture groups in the replacement string with dollar signs ($).
+// "Code Camp".replace(/(\w+)\s(\w+)/, '$2 $1');
+// The replace call would return the string Camp Code.
+//------------------------------------------
