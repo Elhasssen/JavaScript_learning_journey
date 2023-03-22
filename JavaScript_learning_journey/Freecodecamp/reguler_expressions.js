5bf0bc050cb7f93afb6e9219b5730789cd46a875
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-22 19:15:22 +0100

    Reuse Patterns Using Capture Groups

--- JavaScript_learning_journey/Freecodecamp/reguler_expressions.js
+++ JavaScript_learning_journey/Freecodecamp/reguler_expressions.js
@@ -265,4 +265,20 @@
 // let testRegex = /P(engu|umpk)in/;
 // testRegex.test(testStr);
 // the test will return true
+// --------------------------------
+// Reuse Patterns Using Capture Groups
+// Capture groups are constructed by enclosing the regex pattern to be captured in parentheses. 
+// In this case, the goal is to capture a word consisting of alphanumeric characters so the 
+// capture group will be \w+ enclosed by parentheses: /(\w+)/.
 
+// The substring matched by the group is saved to a temporary "variable", 
+// which can be accessed within the same regex using a backslash and the number of the 
+// capture group (e.g. \1). Capture groups are automatically numbered by the position of 
+// their opening parentheses (left to right), starting at 1.
+// 
+// let repeatStr = "row row row your boat";
+// let repeatRegex = /(\w+) \1 \1/;
+// repeatRegex.test(repeatStr); // Returns true
+// repeatStr.match(repeatRegex); // Returns ["row row row", "row"]
+//----------------------------------------
+// 
