d9c789bb973b5db043b3c6b0acd2eed866725b74
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-24 18:44:09 +0100

    Use Caution When Reinitializing Variables Inside a Loop

--- JavaScript_learning_journey/Freecodecamp/debugging.js
+++ JavaScript_learning_journey/Freecodecamp/debugging.js
@@ -50,5 +50,11 @@
 // JavaScript indexing starts at zero, not one, which means the last index is always one less than 
 // the length of the item. If you try to access an index equal to the length, the program may throw 
 // an "index out of range" reference error or print undefined.
-
-
+//--------------------------------------
+// Use Caution When Reinitializing Variables Inside a Loop
+// sometimes it's necessary to save information, increment counters, or re-set variables within a loop. 
+// A potential issue is when variables either should be reinitialized, and aren't, or vice versa. 
+// This is particularly dangerous if you accidentally reset the variable being used for the terminal 
+// condition, causing an infinite loop.
+// Printing variable values with each cycle of your loop by using console.log() can uncover buggy 
+// behavior related to resetting, or failing to reset a variable.
