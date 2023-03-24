2dc78bae7abb7dd179c11b36bd24ae43c15431ee
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-24 17:28:53 +0100

    the JavaScript Console

---
+++ JavaScript_learning_journey/Freecodecamp/debugging.js
@@ -0,0 +1,8 @@
+// Use the JavaScript Console to Check the Value of a Variable
+// The console.log() method, which "prints" the output of what's within its parentheses to the 
+// console, will likely be the most helpful debugging tool. Placing it at strategic points in your 
+// code can show you the intermediate values of variables. It's good practice to have an idea of 
+// what the output should be before looking at what it is. Having check points to see the status of 
+// your calculations throughout your code will help narrow down where the problem is.
+// console.log('Hello world!');
+// ------------------------------
