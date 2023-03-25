1336f324532095a921665682a7bd262f7eba0435
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-25 21:06:44 +0100

    Access an Array's Contents Using Bracket Notation

---
+++ JavaScript_learning_journey/Freecodecamp/basic_data_structures.js
@@ -0,0 +1,18 @@
+// Use an Array to Store a Collection of Data
+// All arrays have a length property, which as shown above, can be very easily accessed with the 
+// syntax Array.length. A more complex implementation of an array can be seen below. This is 
+// known as a multi-dimensional array, or an array that contains other arrays. Notice that this 
+// array also contains JavaScript objects, which we will examine very closely in our next section, 
+// but for now, all you need to know is that arrays are also capable of storing complex objects.
+// let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+// console.log(simpleArray.length);
+// ----------------------------------------------
+// Access an Array's Contents Using Bracket Notation
+// The fundamental feature of any data structure is, of course, the ability to not only store data, 
+// but to be able to retrieve that data on command. So, now that we've learned how to create an array, 
+// let's begin to think about how we can access that array's information.
+// let ourArray = ["a", "b", "c"];
+// ourArray[1] = "not b anymore";
+// Using bracket notation, we have now reset the item at index 1 from the string b, to not b anymore. 
+// Now ourArray is ["a", "not b anymore", "c"].
+// -------------------------------------
