6ca828b8bde3dc8dd20324e316f85d200a345037
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-26 21:30:57 +0100

    Add Items Using splice()

--- JavaScript_learning_journey/Freecodecamp/basic_data_structures.js
+++ JavaScript_learning_journey/Freecodecamp/basic_data_structures.js
@@ -39,3 +39,12 @@
 // array.splice(2, 2);
 // Here we remove 2 elements, beginning with the third element (at index 2). array would have the 
 // value ['today', 'was', 'great'].
+//---------------------------------------------
+// Add Items Using splice()
+// const numbers = [10, 11, 12, 12, 15];
+// const startIndex = 3;
+// const amountToDelete = 1;
+// numbers.splice(startIndex, amountToDelete, 13, 14);
+// console.log(numbers);
+// The second occurrence of 12 is removed, and we add 13 and 14 at the same index. 
+// The numbers array would now be [ 10, 11, 12, 13, 14, 15 ].
