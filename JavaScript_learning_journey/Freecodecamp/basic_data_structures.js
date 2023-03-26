8f4d2774b154b80a7d498e7cdc6f553f2d2aa1bd
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-26 21:13:57 +0100

    Remove Items Using splice()

--- JavaScript_learning_journey/Freecodecamp/basic_data_structures.js
+++ JavaScript_learning_journey/Freecodecamp/basic_data_structures.js
@@ -31,3 +31,11 @@
 // }
 
 // console.log(popShift(['challenge', 'is', 'not', 'complete']));
+// ---------------------------------------
+// Remove Items Using splice()
+// splice()'s first parameter represents the index on the array from which to begin removing elements, 
+// while the second parameter indicates the number of elements to delete
+// let array = ['today', 'was', 'not', 'so', 'great'];
+// array.splice(2, 2);
+// Here we remove 2 elements, beginning with the third element (at index 2). array would have the 
+// value ['today', 'was', 'great'].
