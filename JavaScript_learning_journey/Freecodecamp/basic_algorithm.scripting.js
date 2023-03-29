802ab4ebb529e7b524bbe7a29ef00bf2c8bf4af0
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-29 21:59:29 +0100

    Falsy Bouncer

--- JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
+++ JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
@@ -159,3 +159,19 @@
   
 //   console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
 //-----------------------------------------
+// Falsy Bouncer
+// Remove all falsy values from an array. Return a new array; do not mutate the original array.
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+// function bouncer(arr) {
+//     let newArr = [];
+//     for (let i =0; i<arr.length;i++){
+//       if (arr[i] ){
+//         newArr.push(arr[i]);
+//       } else {
+        
+//       }
+//     }
+//     return newArr;
+//   }
+  
+//   console.log(bouncer([7, "ate", "", false, 9]));
