501b49fc94e9a11f698c1c9b1c4e9fb53d03467f
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-29 02:50:46 +0100

    Reverse a String

---
+++ JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
@@ -0,0 +1,18 @@
+// Convert Celsius to Fahrenheitfunction 
+// convertCtoF(celsius) {
+//     let fahrenheit = celsius* 9/5 + 32;
+//     return fahrenheit ;
+//   }
+  
+//   convertCtoF(30);
+// -----------------------------------
+// Reverse a String
+// function reverseString(str) {
+//     let nStr = '';
+//     for (let i = str.length - 1; i >=  0; i --){
+//       nStr += str[i];
+//     }
+//     return nStr;
+//   }
+  
+//   console.log(reverseString("hello"))
