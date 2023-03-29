d74d0646bd77e8f656f6c184b4bb3a6cb4d4569e
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-29 21:25:17 +0100

    Title Case a Sentence

--- JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
+++ JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
@@ -132,5 +132,14 @@
 //   }
   
 //   booWho(null);
-
+// Title Case a Sentence
+// function titleCase(str) {
+//     const newTitle = str.split(" ");
+//     const updatedTitle = [];
+//     for (let st in newTitle) {
+//       updatedTitle[st] = newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+//     }
+//     return updatedTitle.join(" ");
+//   }
+//   titleCase("I'm a little tea pot");
 
