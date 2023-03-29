b048979d4a42995b30e3ff4dab841947672a70f5
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-29 20:00:30 +0100

    Return Largest Numbers in Arrays

--- JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
+++ JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
@@ -43,3 +43,23 @@
 //   console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
 //   findLongestWordLength("The quick brown fox jumped over the lazy dog");
 // ----------------------------------------------
+// Return Largest Numbers in Arrays
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, 
+// the provided array will contain exactly 4 sub-arrays.
+// function largestOfFour(arr) {
+//     const maxArr = [];
+//     for (let i =0; i < arr.length; i++)
+//     {
+//       let max = arr[i][0];
+//       for (let j = 1; j < arr[i].length; j++){
+//         if (arr[i][j] > max){
+//           max = arr[i][j];
+//         }
+//       }
+//       maxArr.push(max);
+//     }
+//     return maxArr;
+//   }
+  
+//   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);*
+// ------------------------------------------------
