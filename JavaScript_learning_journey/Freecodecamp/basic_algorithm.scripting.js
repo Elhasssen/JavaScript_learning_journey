4696cd5baca2303c4a47bcd94c54b52bca35e9f6
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-29 21:46:24 +0100

    Slice and Splice

--- JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
+++ JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
@@ -142,4 +142,20 @@
 //     return updatedTitle.join(" ");
 //   }
 //   titleCase("I'm a little tea pot");
-
+// -----------------------------------
+// Slice and Splice
+// You are given two arrays and an index.
+// Copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+// function frankenSplice(arr1, arr2, n) {
+//     let ar2 = arr2.slice();
+//     for (let i = 0; i < arr1.length;i++){
+//       ar2.splice(n,0,arr1[i]);
+//       n++
+//     }
+//     return ar2;
+//   }
+  
+//   console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+//-----------------------------------------
