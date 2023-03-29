3379e1426110afb88342db7c8ce1dfdd77dfd634
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-29 23:18:44 +0100

    Mutations

--- JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
+++ JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
@@ -191,3 +191,15 @@
 //   getIndexToIns([5, 3, 20, 3], 5);
 // Return the lowest index at which a value (second argument) should be inserted into an array 
 // (first argument) once it has been sorted. The returned value should be a number.
+// Mutations 
+// Return true if the string in the first element of the array contains all of the letters of the 
+// string in the second element of the array.
+// function mutation(arr) {
+//   const test = arr[1].toLowerCase();
+//   const target = arr[0].toLowerCase();
+//   for (let i = 0; i < test.length; i++) {
+//     if (target.indexOf(test[i]) < 0) return false;
+//   }
+//   return true;
+// }
+// console.log(mutation(["Noel", "Ole"]));
