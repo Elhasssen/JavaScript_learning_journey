825e4c8402ab4513907608273eb3120b25697906
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-30 21:05:45 +0100

    Use a Constructor to Create Objects

--- JavaScript_learning_journey/Freecodecamp/object_oriented_programing.js
+++ JavaScript_learning_journey/Freecodecamp/object_oriented_programing.js
@@ -46,3 +46,17 @@
 // Constructors use the keyword this to set properties of the object they will create. Inside the constructor,
 //  this refers to the new object it will create.
 // Constructors define properties and behaviors instead of returning a value as other functions might.
+// ----------------------------------------------------
+// Use a Constructor to Create Objects
+// function Bird() {
+//     this.name = "Albert";
+//     this.color  = "blue";
+//     this.numLegs = 2;
+//   }
+  
+//   let blueBird = new Bird();
+//   otice that the new operator is used when calling a constructor. This tells JavaScript to create a 
+//   new instance of Bird called blueBird. Without the new operator, this inside the constructor would 
+//   not point to the newly created object, giving unexpected results. Now blueBird has all the 
+//   properties defined inside the Bird constructor
+// -----------------------------------------------
