727adf99e523f42937589ac36779517644d0c53e
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-30 18:52:47 +0100

    Chunky Monkey

--- JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
+++ JavaScript_learning_journey/Freecodecamp/basic_algorithm.scripting.js
@@ -203,3 +203,24 @@
 //   return true;
 // }
 // console.log(mutation(["Noel", "Ole"]));
+//---------------------------------------
+//Chunky Monkey
+// Write a function that splits an array (first argument) into groups the length of size 
+// (second argument) and returns them as a two-dimensional array.
+// function chunkArrayInGroups(arr, size) {
+//     // Break it up.
+//     const newArr = [];
+//     let i = 0;
+  
+//     while (i < arr.length) {
+//       newArr.push(arr.slice(i, i + size));
+//       console.log(arr.slice(i, i + size));
+//       console.log(newArr);
+//       console.log(i)
+//       i += size;
+//       console.log(i)
+//     }
+//     return newArr;
+//   }
+  
+//   chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
