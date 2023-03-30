2deb0990ddd8b9e5c7dfd0018e92f643035010aa
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-03-30 20:09:07 +0100

    Create a Basic JavaScript Object

---
+++ JavaScript_learning_journey/Freecodecamp/object_oriented_programing.js
@@ -0,0 +1,8 @@
+// creating a basic JS Object
+// let duck = {
+//   name: "Aflac",
+//   numLegs: 2
+// };
+// Objects in JavaScript are used to model real-world objects, giving them properties and behavior 
+// just like their real-world counterparts. Here's an example using these concepts to create a 
+// duck object
