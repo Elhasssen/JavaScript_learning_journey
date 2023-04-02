133d5fba0781cf6cfa6aa3e0ba56a98966469d4b
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-02 04:47:02 +0100

    Use a Mixin to Add Common Behavior Between Unrelated Objects

--- JavaScript_learning_journey/Freecodecamp/object_oriented_programing.js
+++ JavaScript_learning_journey/Freecodecamp/object_oriented_programing.js
@@ -266,3 +266,27 @@
 // Animal => eat() is also defined, but JavaScript stopped searching before reaching this level.
 // Object => JavaScript stopped searching before reaching this level.
 // -----------------------------------------------------
+// Use a Mixin to Add Common Behavior Between Unrelated Objects.
+// As you have seen, behavior is shared through inheritance. However, there are cases when inheritance 
+// is not the best solution. Inheritance does not work well for unrelated objects like Bird and Airplane. 
+// They can both fly, but a Bird is not a type of Airplane and vice versa.
+// let flyMixin = function(obj) {
+//     obj.fly = function() {
+//       console.log("Flying, wooosh!");
+//     }
+//   };
+
+//   let bird = {
+//     name: "Donald",
+//     numLegs: 2
+//   };
+  
+//   let plane = {
+//     model: "777",
+//     numPassengers: 524
+//   };
+  
+//   flyMixin(bird);
+//   flyMixin(plane);
+//   bird.fly();
+// plane.fly();
