897aa8c4463873f67bbfbc7ccae7aabc107b012e
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-03 22:49:06 +0100

    Implement the filter Method on a Prototype

--- JavaScript_learning_journey/Freecodecamp/functional_programming.js
+++ JavaScript_learning_journey/Freecodecamp/functional_programming.js
@@ -128,3 +128,18 @@
 //   console.log(usersUnder30); 
 // The console would display the value [ { name: 'Amy', age: 20 }, { name: 'camperCat', age: 10 } ].
 //-------------------------------------------------------------------
+// Implement the filter Method on a Prototype
+// Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). 
+// You should not use the built-in filter method. The Array instance can be accessed in the myFilter 
+// method using this.
+// Array.prototype.myFilter = function(callback) {
+//     const newArray = [];
+//       // Only change code below this line
+//         for (let i = 0; i < this.length; i++) {
+//       if (callback(this[i], i, this) == true) {
+//         newArray.push(this[i]);
+//       }
+//     }
+//       // Only change code above this line
+//       return newArray;
+//   };
--- JavaScript_learning_journey/Freecodecamp/test/myscripts.js
+++ JavaScript_learning_journey/Freecodecamp/test/myscripts.js
@@ -1,7 +1,12 @@
-const kvArray = [
-    { key: 1, value: 10 },
-    { key: 2, value: 20 },
-    { key: 3, value: 30 },
-  ];
-  
-  const reformattedArray = kvArray.map(({ key, value }) => ({ key: value }));
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+    // Only change code below this line
+      for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this) == true) {
+      newArray.push(this[i]);
+    }
+  }
+    // Only change code above this line
+    return newArray;
+};
+console.log([23, 65, 98, 5, 13].myFilter(item => item % 2 ) )
