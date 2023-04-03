fc8ebb3300a79d0723c5c6624646c9954122cf33
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-03 08:27:04 +0100

    Pass Arguments to Avoid External Dependence in a Function

--- JavaScript_learning_journey/Freecodecamp/functional_programming.js
+++ JavaScript_learning_journey/Freecodecamp/functional_programming.js
@@ -31,3 +31,6 @@
 // Recall that in functional programming, changing or altering things is called mutation, and the 
 // outcome is called a side effect. A function, ideally, should be a pure function, meaning that it 
 // does not cause any side effects.
+// Avoid Mutations and Side Effects Using Functional Programming
+// function depends on a variable or object being present, then pass that variable or object 
+// directly into the function as an argument.
