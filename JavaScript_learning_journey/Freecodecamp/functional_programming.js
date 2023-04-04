0a8a4a88d7df2d76119a13427073214d0ce33528
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-04 03:50:54 +0100

    Combine Two Arrays Using the concat Method

--- JavaScript_learning_journey/Freecodecamp/functional_programming.js
+++ JavaScript_learning_journey/Freecodecamp/functional_programming.js
@@ -154,3 +154,14 @@
 //   const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 //   sliceArray(inputAnim, 1, 3);
 // ----------------------------------------------------
+// Combine Two Arrays Using the concat Method
+// function nonMutatingConcat(original, attach) {
+//     // Only change code below this line
+  
+//     return original.concat(attach);
+//     // Only change code above this line
+//   }
+  
+//   const first = [1, 2, 3];
+//   const second = [4, 5];
+//   nonMutatingConcat(first, second);
