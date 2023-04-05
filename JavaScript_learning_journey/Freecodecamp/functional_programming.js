f656456d1ea4b0eb0dd2da73d26f96b7d2071f7b
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-05 03:33:30 +0100

    Combine an Array into a String Using the join Method

--- JavaScript_learning_journey/Freecodecamp/functional_programming.js
+++ JavaScript_learning_journey/Freecodecamp/functional_programming.js
@@ -260,3 +260,15 @@
 //   }
   
 // console.log(splitify("Hello World,I-am code"));
+// ---------------------------------------------
+// Combine an Array into a String Using the join Method
+// The join method is used to join the elements of an array together to create a string. It takes an 
+// argument for the delimiter that is used to separate the array elements in the string.
+// function sentensify(str) {
+//     // Only change code below this line
+//     return str.split(/\W/).join(' ')
+  
+//     // Only change code above this line
+//   }
+  
+//   sentensify("May-the-force-be-with-you");
