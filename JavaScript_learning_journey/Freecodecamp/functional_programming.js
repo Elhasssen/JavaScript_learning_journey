ebda8eb2f196980c159a04f61523a9c213478eea
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-05 04:35:58 +0100

    Introduction to Currying and Partial Application

--- JavaScript_learning_journey/Freecodecamp/functional_programming.js
+++ JavaScript_learning_journey/Freecodecamp/functional_programming.js
@@ -299,3 +299,37 @@
 //   return currentValue < 10;
 // });
 // The some method would return true.
+//-------------------------------------------------------------
+// Introduction to Currying and Partial Application
+// The arity of a function is the number of arguments it requires. Currying a function means to 
+// convert a function of N arity into N functions of arity 1.
+// In other words, it restructures a function so it takes one argument, then returns another 
+// function that takes the next argument, and so on.
+// function unCurried(x, y) {
+//     return x + y;
+//   }
+  
+//   function curried(x) {
+//     return function(y) {
+//       return x + y;
+//     }
+//   }
+  
+//   const curried = x => y => x + y
+  
+//   curried(1)(2)
+//   Similarly, partial application can be described as applying a few arguments to a function 
+//   at a time and returning another function that is applied to more arguments. 
+//   function impartial(x, y, z) {
+//     return x + y + z;
+//   }
+  
+//   const partialFn = impartial.bind(this, 1, 2);
+//   partialFn(10); // 13
+//   So, here’s the difference between them.
+
+// Currying: A function returning another function that might return another function, but every 
+// returned function must take only one parameter at a time.
+// Partial application: A function returning another function that might return another function, 
+// but each returned function can take several parameters.
+//---------------------------------------------------
