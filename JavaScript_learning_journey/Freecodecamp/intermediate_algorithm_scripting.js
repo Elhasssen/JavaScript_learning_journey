f037c858df9716981a99cb8e06b63ffae75663b6
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-05 21:08:51 +0100

    Sum All Numbers in a Range

---
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -0,0 +1,16 @@
+// Sum All Numbers in a Range
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of 
+// all the numbers between them. The lowest number will not always come first.
+// function sumAll(arr) {
+//     let max = Math.max(...arr);
+//     let min = Math.min(...arr);
+//     let sum = 0;
+//     while (max >= min){
+//       sum = sum + max;
+//       max = max - 1;
+//     }
+//     return sum;
+//   }
+  
+//   console.log(sumAll([1, 4]));
+// ------------------------------------------------
