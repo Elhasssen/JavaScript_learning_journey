25cedf19dbade8a36b7a1e5fbeb4cdd793e10626
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-06 21:25:40 +0100

    Seek and Destroy

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -40,4 +40,22 @@
 //   }
   
 //   console.log(destroyer([ 1,2, 3, 1, 2, 3], 2, 3));
+// ------------------other ways ---------------------
+// function destroyer(arr) {
+//     const valsToRemove = Array.from(arguments).slice(1);
+//     return arr.filter(function(val) {
+//       return !valsToRemove.includes(val);
+//     });
+//   }
+//   Declare a variable named valsToRemove and set it equal to a new Array object from() the arguments 
+//   passed into the function. Use the slice() method on the array of arguments, starting from the 
+//   second index, 1.
+//   Return the filtered array, using includes() in the callback function to check if val is not 
+//   in valsToRemove; returning true to keep the value in the original array or false to remove it.  
+// // ----------------- other ways -----------------------
+// function destroyer(arr, ...valsToRemove) {
+//     return arr.filter(elem => !valsToRemove.includes(elem));
+//   }
+//   Using spread operator to retrieve the arguments.
+// Return the filtered array, using includes()
 // -----------------------------------------------------
