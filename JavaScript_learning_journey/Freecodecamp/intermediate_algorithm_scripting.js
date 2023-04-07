3202d55e51f0c1e2068568f8e37996cc861ecbe9
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-07 23:31:21 +0100

    Spinal Tap Case

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -78,3 +78,14 @@
 //   }
   
 //   console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+//----------------------------------
+// Spinal Tap Case
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+// function spinalCase(str) {
+//     return str.split(/\s|_|(?=[A-Z])/);
+//   }
+  
+// console.log(spinalCase('thisIsSpinalTap'));
+//-----------------------------------------
+// 
+
