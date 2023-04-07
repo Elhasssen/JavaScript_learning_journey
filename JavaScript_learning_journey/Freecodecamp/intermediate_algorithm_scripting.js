3e57ac832e289741e7cce8eeec17f43f78348deb
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-07 22:21:33 +0100

    Wherefore art thou

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -59,3 +59,22 @@
 //   Using spread operator to retrieve the arguments.
 // Return the filtered array, using includes()
 // -----------------------------------------------------
+// Wherefore art thou
+// Make a function that looks through an array of objects (first argument) and returns an array of all 
+// objects that have matching name and value pairs (second argument). Each name and value pair of the 
+// source object has to be present in the object from the collection if it is to be included in the 
+// returned array.
+// function whatIsInAName(collection, source) {
+//     const sourceKeys = Object.keys(source);
+//     return collection.filter(obj => {
+//       for (let i = 0; i < sourceKeys.length; i++) {
+//         if (!obj.hasOwnProperty(sourceKeys[i]) ||
+//             obj[sourceKeys[i]] !== source[sourceKeys[i]]) {
+//           return false;
+//         }
+//       }
+//       return true;
+//     });
+//   }
+  
+//   console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
