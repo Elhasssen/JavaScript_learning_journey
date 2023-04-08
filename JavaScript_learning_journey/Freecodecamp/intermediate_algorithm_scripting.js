27af0c1c799c852ade0ecb24a61f0c6b970ea8f8
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-08 06:11:01 +0100

    Search and Replace

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -105,3 +105,21 @@
   
 //   translatePigLatin("consonant");
 //--------------------------------
+// Search and Replace
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+// First argument is the sentence to perform the search and replace on.
+// Second argument is the word that you will be replacing (before).
+// function myReplace(str, before, after) {
+//     // Check if first character of argument "before" is a capital or lowercase letter and change the first character of argument "after" to match the case
+//     if (/^[A-Z]/.test(before)) {
+//       after = after[0].toUpperCase() + after.substring(1)
+//     } else {
+//       after = after[0].toLowerCase() + after.substring(1)
+//     }
+  
+//     // return string with argument "before" replaced by argument "after" (with correct case)
+//     return str.replace(before, after);
+//   }
+  
+//   console.log(myReplace("I think we should look up there", "up", "Down"));
+//--------------------------------------
