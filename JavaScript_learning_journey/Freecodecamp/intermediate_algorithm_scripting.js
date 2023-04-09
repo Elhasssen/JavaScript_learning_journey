aa8e43818d45fd3f8d1967271c8ca9fa4f031264
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-09 23:38:27 +0100

    Sum All Odd Fibonacci Numbers

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -218,3 +218,22 @@
 
 // console.log(convertHTML("Hamburgers < Pizza < Tacos"));
 //-------------------------------------------------------
+// Sum All Odd Fibonacci Numbers
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+// function sumFibs(num) {
+//   let prevNumber = 0;
+//   let currNumber = 1;
+//   let result = 0;
+//   while (currNumber <= num) {
+//     if (currNumber % 2 !== 0) {
+//       result += currNumber;
+//     }
+//     currNumber += prevNumber;
+//     prevNumber = currNumber - prevNumber;
+//   }
+
+//   return result;
+// }
+
+
+// sumFibs(4);
