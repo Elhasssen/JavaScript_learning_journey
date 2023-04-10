5150161166d6617215cfe690519ead5d917bc247
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-10 04:27:38 +0100

    Sum All Primes

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -237,3 +237,29 @@
 
 
 // sumFibs(4);
+// ------------------------------------
+// Sum All Primes
+// A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 
+// 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is 
+// divisible by 1, 2 and 4.
+// function sumPrimes(num) {
+//   // Helper function to check primality
+//   function isPrime(num) {
+//     const sqrt = Math.sqrt(num);
+//     for (let i = 2; i < num; i++) {
+//       if (num % i == 0){return false;}
+        
+//     }
+//     return true;
+//   }
+
+//   // Check all numbers for primality
+//   let sum = 0;
+//   for (let i = 2; i <= num; i++) {
+//     if (isPrime(i))
+//       sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(sumPrimes(2));
