307765e9c3b05666c5e09b687b9cb6a500d93711
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-12 08:29:21 +0100

    Drop it

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -290,3 +290,15 @@ passive agressive
 // }
 
 // smallestCommons([1, 5]);
+// -----------------------------
+// function dropElements(arr, func) {
+//   while (arr.length > 0 && !func(arr[0])) {
+//     arr.shift();
+//   }
+//   return arr;
+// }
+
+// // test here
+// dropElements([1, 2, 3, 4], function(n) {
+//   return n >= 3;
+// });
