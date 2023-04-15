e6ea0ee9bd273ccf9f4cebe8fa23ae49ea1eace7
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-15 04:03:31 +0100

    Make a Person

--- JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
+++ JavaScript_learning_journey/Freecodecamp/intermediate_algorithm_scripting.js
@@ -350,3 +350,35 @@ passive agressive
 //       return undefined;
 //     return first + second;
 //   }
+//-----------------------------------
+// make a person
+// const Person = function(firstAndLast) {
+//   let fullName = firstAndLast;
+
+//   this.getFirstName = function() {
+//     return fullName.split(" ")[0];
+//   };
+
+//   this.getLastName = function() {
+//     return fullName.split(" ")[1];
+//   };
+
+//   this.getFullName = function() {
+//     return fullName;
+//   };
+
+//   this.setFirstName = function(name) {
+//     fullName = name + " " + fullName.split(" ")[1];
+//   };
+
+//   this.setLastName = function(name) {
+//     fullName = fullName.split(" ")[0] + " " + name;
+//   };
+
+//   this.setFullName = function(name) {
+//     fullName = name;
+//   };
+// };
+
+// const bob = new Person('Bob Ross');
+// bob.getFirstName();
