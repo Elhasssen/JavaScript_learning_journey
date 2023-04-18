417459006dbc2d8ded954a93f3c202197ad9cbe9
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-04-18 05:49:56 +0100

    cipher

--- JavaScript_learning_journey/Freecodecamp/functional_programming.js
+++ JavaScript_learning_journey/Freecodecamp/functional_programming.js
@@ -333,15 +333,5 @@
 // Partial application: A function returning another function that might return another function, 
 // but each returned function can take several parameters.
 //---------------------------------------------------
-// Arguments Optional
-// function addTogether() {
-//     const [first, second] = arguments;
-//     if (typeof(first) !== "number")
-//       return undefined;
-//     if (arguments.length === 1)
-//       return (second) => addTogether(first, second);
-//     if (typeof(second) !== "number")
-//       return undefined;
-//     return first + second;
-//   }
+
 //----------------------------------------
--- JavaScript_learning_journey/Freecodecamp/test/myscripts.js
+++ JavaScript_learning_journey/Freecodecamp/test/myscripts.js
@@ -1,12 +1,4 @@
-Array.prototype.myFilter = function(callback) {
-  const newArray = [];
-    // Only change code below this line
-      for (let i = 0; i < this.length; i++) {
-    if (callback(this[i], i, this) == true) {
-      newArray.push(this[i]);
-    }
-  }
-    // Only change code above this line
-    return newArray;
-};
-console.log([23, 65, 98, 5, 13].myFilter(item => item % 2 ) )
+let str = 'abcdefghijklmnopqrstuvwxrz'
+let x = str.indexOf('s')
+let res =Math.abs(str.length - parseInt(str.indexOf('c')) -  13)
+console.log(str[res])
