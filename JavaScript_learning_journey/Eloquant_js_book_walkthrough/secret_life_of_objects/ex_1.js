43df91515b4ee02a6ac1d7fe6151d01eff37035d
ElHassen <Boumeddiene.elhassen@gmail.com>
2023-06-16 14:55:39 +0100

    hellllllo

---
+++ JavaScript_learning_journey/Eloquant_js_book_walkthrough/secret_life_of_objects/ex_1.js
@@ -0,0 +1,31 @@
+class Group{
+    constructor(){
+        this.arr = [];
+    }
+
+    add(val){
+        if (!this.has(val)){
+            this.arr.push(val)
+        }
+    }
+    delete(val){
+        this.arr = this.arr.filter(x => x != val);
+    }
+    static from(input){
+        let group = new Group;
+        for (let v of input){
+            group.add(v);
+        }
+        return group
+    }
+    has(value){
+        return this.arr.includes(value)
+    }
+
+}
+
+let ha =  Group.from([1,2,3]);
+
+
+
+console.log(ha.has(1))
